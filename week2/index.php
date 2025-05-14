03e4969ed11bffc045c81fee8349f52f7741d48d
Qianyi <n01718099@humber.ca>
2025-05-14 16:36:50 -0400

    Updated local changes from XAMPP htdocs

---
+++ week2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Week 2</title>
+</head>
+<body>
+<!--Learning Echo -->
+  <?php
+  echo "<h1>Hello World!</h1>";
+  echo "<p>
+        My name is Qianyi
+        </p>"
+
+  ?>
+
+
+<!-- Learning Variables-->
+  <?php 
+  $fname = "Qianyi";
+  $lname = "Dong";
+
+  echo"<h1>Hello $fname $lname </h1>";
+  
+  ?>
+</body>
+</html>
